21a5a46a88879159369fd3fa12937ea1bd30b011
Yang-heesu <hsge0602@gmail.com>
2020-07-22 17:56:47 +0900

    해당되는 index의 data보이게 만들기

--- src/frontend/src/components/Detail.vue
+++ src/frontend/src/components/Detail.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        Detail
+        <div>{{data.writer}}</div>
+        <div>{{data.title}}</div>
+        <div>{{data.content}}</div>
     </div>
 </template>
 <script>
+    import data from '../data'
+
     export default {
-        name: 'Detail'
+        name: 'Detail',
+        data(){
+            const index = this.$route.params.contentId
+            return {
+                data: data[index]
+            }
+        }
     }
 </script>
