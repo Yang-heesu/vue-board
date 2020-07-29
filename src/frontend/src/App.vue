d1cbc303f956be220b3c484becbd3c8b984453fb
Yang-heesu <hsge0602@gmail.com>
2020-07-29 11:40:11 +0900

    bootstrap 테이블

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
     <Header />
-    <!--img alt="Vue logo" src="./assets/logo.png">-->
     <router-view/>
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-  import request from 'request'
+  //import request from 'request'
   import Header from './components/Header'
 
   export default {
     name: 'App',
     components: {
       Header,
-    },
+    }/*,
     mounted(){
         request('http://localhost:8080/api/hello', function (error, response, body) {
             window.console.log('error: ', error);
             window.console.log('statusCode: ', response && response.statusCode);
             window.console.log('body: ', body);
         });
-     }
+     }*/
   }
 </script>
 
---
+++ src/frontend/src/components/Board.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+    </div>
+</template>
+<script>
+    import data from '../data'
+    //아래와 같음
+    //@/data
+    //../data/index.js
+
+    export default {
+        name: 'Board',
+        data(){
+            return {
+                fields: ['content_id', 'title', 'created_at'],
+                items: data.Content
+            }
+        }
+    }
+</script>
