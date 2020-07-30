59d0e9d91cecbfa8eb7997c6385bd256f02eed57
Yang-heesu <hsge0602@gmail.com>
2020-07-30 11:13:56 +0900

    댓글 나타나게 만들기

---
+++ src/frontend/src/components/CommentList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div :key="item.commend_id" v-for="item in comments">
+            <div>{{item.context}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import data from '../data'
+
+    export default {
+        name: "CommentList",
+        props: {
+            contentId: Number
+        },
+        data() {
+            return {
+                comments: data.Comment.filter(item => item.content_id === this.contentId)
+            }
+        }
+    }
+</script>
